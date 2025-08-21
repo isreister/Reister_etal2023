c5152eac1c38195992dc9774fa3d707c3ea15073
Isaac Reister <isreister@alaska.edu>
2025-08-20 17:21:32 -0700

    Updated satillite retrieval scripts

--- WMSsatretrieval.m
+++ WMSsatretrieval.m
@@ -1,10 +1,27 @@
+%This function is saved to isreister/Reister_etal2023 github account as
+%WMSsatretrieval.m
 
-function WMSsatretrieval(land,t,i)
+%this is a function that is used within the code script 'mydates_satretrieval_v2.m'. 
+% Inputs for the function are a land mask (land), a list of dates (t), and an index 
+% value of 1,2,3,4,...length(t). The function uses an API to recover the cloud_fraction for 
+% aqua and terra sattillite images of the geospatial location defined by latlim and lonlim. 
+% Future users need to modify latlim and lonlim to match their region of interest. The function 
+% uses a user defined region of interest (ROI) within the latlim and lonlim to decide which satillite 
+% image is better between the aqua and terra images for a given date. The ROI is specified by the user
+% on the first run through the code. ROI was useful for the Copper River because I cared most about 
+% if the region near the copper river mouth was clear of clouds, even though the satillite image was 
+% about 4 times as big as that ROI. The best satillite image (if there is one) is saved to a directory
+% that is automatically created. Beware if you choose lots of dates to consider, this directory can get very large.
+
+
+
+function WMSsatretrievalv2(land,t,i)
 
 clearvars -global -except land latlim lonlim imagelength url1 url2 t i
 currentdate=t(i);
 writtenmonth=datetime(t(i),'Format','MMMM d, yyyy');
-strcurrentdate=string(currentdate)
+strcurrentdate=string(currentdate);
+disp(strcurrentdate)
 charcurrentdate=char(strcurrentdate);
 charmonth=char(writtenmonth);
 year=charcurrentdate(1:4);
@@ -14,23 +31,43 @@ day=charcurrentdate(9:10);
 
 
 
+url1 = 'https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?SERVICE=WMS&REQUEST=GetMap&VERSION=1.3.0&LAYERS=MODIS_Terra_MODIS_Terra_Cloud_Fraction_Day&STYLES=&FORMAT=image%2Fpng&TRANSPARENT=true&HEIGHT=256&WIDTH=256&TIME=';
+url2= strcat(strcurrentdate, '&CRS=EPSG:4326&BBOX=-22.5,0,0,22.5'); 
+url= char(strcat(url1,url2));
+%url = 'https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?'; %in theory this should work..it's the same as the url used to the land mask and that works fine.
+      
 
-url = 'https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?';
 info = wmsinfo(url);
-modis = info.Layer.refine('modis');
-Cloud_Fraction = modis.refine('true color','SearchField','LayerTitle');
-Cloud_Fraction_aqua = Cloud_Fraction.refine('Aqua','SearchField','LayerTitle');
+modis = info.Layer.refine('modis'); %this is all the MODIS satilite layers.
+Cloud_Fraction = modis.refine('Cloud_Fraction','SearchField','LayerTitle');%all of the cloud_fraction layers
+Cloud_Fraction_aqua = Cloud_Fraction.refine('Aqua','SearchField','LayerTitle'); %all of the aqua cloud fraction layers
 Cloud_Fraction_terra = Cloud_Fraction.refine('Terra','SearchField','LayerTitle');
-Cloud_Fraction_aqua_day = Cloud_Fraction_aqua.refine('Day','SearchField','LayerTitle');
-Cloud_Fraction_terra_day = Cloud_Fraction_terra.refine('Day','SearchField','LayerTitle');
+Cloud_Fraction_aqua_day = Cloud_Fraction_aqua.refine('Day','SearchField','LayerTitle');% the day portion of the cloud fraction layers?
+Cloud_Fraction_terra_day = Cloud_Fraction_terra.refine('Day','SearchField','LayerTitle'); %
 latlim = [59.5, 60.6];
 lonlim = [-147,-144];
 imagelength = 2048;
-[A] = wmsread(Cloud_Fraction_aqua_day,'ImageHeight',imagelength,'ImageWidth',imagelength,...
+[A] = wmsread(Cloud_Fraction_aqua_day,'ImageHeight',imagelength,'ImageWidth',imagelength,... %now read the part that actually matters for us.
     'Latlim',latlim,'Lonlim',lonlim);
 [B]= wmsread(Cloud_Fraction_terra_day,'ImageHeight',imagelength,'ImageWidth',imagelength,...
     'Latlim',latlim,'Lonlim',lonlim);
 
+
+
+truecolortesting = modis.refine('MODIS_Aqua_CorrectedReflectance_TrueColor','SearchField','LayerTitle');
+[T]= wmsread(truecolortesting,'ImageHeight',imagelength,'ImageWidth',imagelength,...
+    'Latlim',latlim,'Lonlim',lonlim);
+
+
+
+    figure(1) %uncomment to see what you are looking at.
+    clf
+    imshow(T)%original cloud fraction image
+
+    figure(2) %uncomment to see what the heck your looking at.
+    clf
+    imshow(A)%original cloud fraction image
+%%
 clear info;
 %% Decide betweeen Terra and Aqua.
 
@@ -42,130 +79,101 @@ tempCell{1,2}=B;
 
 
 %---------
-%In this section we rotate the image a few times and make black sections of the cloud faction calculation that
-%we do not need (this applies the land mask as well the east of kayak
-%island and west of hinchinbrook island. The seaward edge is approximately
-%the blanked out at the shelf edge.
-    % figure(1)
-    % clf
-    % imshow(A)%original cloud fraction image
-    CustomCloudPercent=nan(1,2);
-for ix=1:length(tempCell)
 
-    C=tempCell{1,ix};
+tempCell{1,1} = A;
+tempCell{1,2} = B;
 
-    C(land)=0;%land mask applied
-%     
-%         figure(2)
-%         clf
-%         imshow(C)
-    
-    C = imrotate(C,27,'crop');
-%         figure(4)
-%         imshow(C)
-    bottomquarter=imagelength-(imagelength/4);
-    C(bottomquarter:imagelength,:,:)=0; %mask of "beyond the shelf edge" applied.
-%         figure(5)
-%         imshow(C)
-    
-    C = imrotate(C,7,'crop');
-%         figure(6)
-%         imshow(C)
-    rightninth=imagelength-(imagelength/9);
-    C(:,rightninth:imagelength,:)=0; %mask of east of kayak island applied
-%         figure(7)
-%         imshow(C)
-    
-    C = imrotate(C,25,'crop');%the crop here is enough to remove "west of hinchinbrook".
-%         figure(8)
-%         imshow(K)
-    %this completes the specification of our bounding box.
-    
-    %----------------
-    Re=C;
-    RED=[uint8(230),uint8(50),uint8(50)].';
-    inRED=find(Re(:,:,1)>RED(1) & Re(:,:,2)<RED(2) & Re(:,:,3)<RED(3));
-    clear Re;
-    % R1=R(:,:,1);%designates the different layers.
-    % R2=R(:,:,2);
-    % R3=R(:,:,3);
-    % R1(inRED)=uint8(255);
-    % R2(inRED)=uint8(255);
-    % R2(inRED)=uint8(255);
-    % R(:,:,1)=R1;
-    % R(:,:,2)=R2;
-    % R(:,:,3)=R3;%makes all the 100% clouds within our "bounding box that is actually created by just removing
-    % %everything we don't want" white
-    % clear R1;
-    % clear R2;
-    % clear R3;
-    %     figure(9)
-    %     clf
-    %     imshow(R)
-    
-    BLACK=[uint8(0),uint8(0),uint8(0)].';
-    inBLACK=find(C(:,:,1)==BLACK(1) & C(:,:,2)==BLACK(2) & C(:,:,3)==BLACK(3));
-    clear C;
-    
-    pixelsthatareblack=length(inBLACK); %this is our total number of pixels that we DO NOT want to consider. Note that
-    %the last value (and max value) here designates the bottom right corner of our image
-    %and is 4194304.Its imporant to know that find is giveing back a vector of
-    %indice values that correspond to the row column space ONLY. it does not
-    %correspond to the row column level space (3D space of the matrix).
-    %Therefor if we want the pixels that are not black, we take the total
-    %number of pixels in row column space and subtract black colored pixels,
-    %leaving behing other colored pixesl, which is, the "bounding box" we want
-    %to consider as 100% for our cloud faction analysis. 
-    clear inBLACK;
-    pixelsthatareNOTblack=(2048.*2048)-pixelsthatareblack;
-    disp(pixelsthatareNOTblack); %this value should not change.
-    disp(length(inRED)); %this value should change
-    CustomCloudPercent(ix)=(length(inRED)./pixelsthatareNOTblack).*100;
-    %This commented section will show that the 100% cloud faction pixels are 
-    %being dectected correctly. They are correct if the red pixels in figure 1
-    %become black pixels in figure 2.The length of InRED gives us how many 100
-    %pixels there there are. Divided by the total number of pixels, gives us
-    %the percentage of the image that registers as 100% cloud faction. This
-    %percentage is what we use to filter out cloudy days.
-%------------
+CustomCloudPercent = nan(1,2);
+
+if i==1
+        C = tempCell{1};
+
+    % Step 1: apply land mask first
+    C(repmat(land,1,1,3)) = 0;
+
+    % Step 2: show image and let user draw polygon
+    % User should draw the polygon around the region of interest within the
+    % satillite image. Don't worry about cloud cover or land. Land will be
+    % masked even if it is part of the region of interest. cloud cover is
+    % delt examined within  each image, and a cloud fraction for the region
+    % of interest is calculated to determin if this satillite image is
+    % worth anything to you.
+    figure(1); clf;
+    imshow(C);
+    title('Draw polygon around region of interest, finish by clicking on your starting point again');
+    h = drawpolygon; % user draws ROI
+    BW = createMask(h); % logical mask of polygon
+    save('polygon_of_interest.mat',"BW")
+else
+    load('polygon_of_interest.mat','"BW"')
 end
+
+for ix = 1:length(tempCell)
+    C = tempCell{ix};
+
+    % Step 1: apply land mask first
+    C(repmat(land,1,1,3)) = 0;
+
+    
+    % Step 2: apply polygon mask
+    C(~repmat(BW,1,1,3)) = 0;
+
+    % Step 3: count red (cloud) pixels inside polygon
+    RED = uint8([230,50,50]);
+    inRED = (C(:,:,1) > RED(1)) & (C(:,:,2) < RED(2)) & (C(:,:,3) < RED(3));
+
+    % Step 4: count valid (non-black) pixels inside polygon
+    BLACK = uint8([0,0,0]);
+    inBLACK = (C(:,:,1)==BLACK(1)) & (C(:,:,2)==BLACK(2)) & (C(:,:,3)==BLACK(3));
+
+    pixelsthatareNOTblack = numel(inBLACK) - nnz(inBLACK);
+
+    % Step 5: calculate cloud fraction
+    CustomCloudPercent(ix) = (nnz(inRED) / pixelsthatareNOTblack) * 100;
+end
+
+%disp(CustomCloudPercent);
+
+%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%
+
+
+   
 aquaorterra=["Aqua" "Terra"]; 
-[mymin,myindi]=min(CustomCloudPercent);
-if mymin<50
-
-bestofaquaandterra=char(aquaorterra(myindi));
-
-true_color = modis.refine(bestofaquaandterra,'true color','SearchField','LayerTitle');
-true_color = true_color.refine(bestofaquaandterra,'SearchField','LayerTitle');
-[A,R] = wmsread(true_color,'ImageHeight',imagelength,'ImageWidth',imagelength,...
-    'Latlim',latlim,'Lonlim',lonlim);
-clear modis;
-f = figure('visible','off');
-geoshow(A,R);
-
-
-root='C:\Users\Isaac\Documents\MATLAB\Research\Figures\API';
-
-path1=[root,'\',year];    
-if ~exist(path1, 'dir')
-       mkdir(path1)
-end
-month=strrep(month,' ',[]); %put this in to take care of the month of may
-%which has a space at it's end.
-path2=[root,'\',year,'\',month];
-if ~exist(path2, 'dir')
+[mymin,myindi]=min(CustomCloudPercent); %Here the we just say "ok given aqua and terra images, which has less clouds".
+if mymin<50 %Here we say Ok given that we are looking at the best image between aqua and terra, is it actually good enough (aka, less than 50% clouds).
+    
+    bestofaquaandterra=char(aquaorterra(myindi));
+    
+    true_color = modis.refine(bestofaquaandterra,'SearchField','LayerTitle');
+    true_color = true_color.refine('TrueColor','SearchField','LayerTitle');
+    [A,R] = wmsread(true_color,'ImageHeight',imagelength,'ImageWidth',imagelength,...
+        'Latlim',latlim,'Lonlim',lonlim);
+    clear modis;
+    f = figure('visible','off');
+    geoshow(A,R);
+    
+    
+    root='C:\Users\funkb\Documents\MATLAB\Research\Figures\Testing'; %@ this is where the images will be saved. These images will be 50 percent clouds or less for the section of the image you specify while cropping the image. If you just want to get 50% or less clouds for a square image, just remove the cropping.
+    
+    path1=[root,'\',year]; 
+    if ~exist(path1, 'dir')
+       mkdir(path1) % beware! this will make folders in your directory specified by root. Which is nice for organization but just be aware of that.
+    end
+    month=strrep(month,' ',[]); %put this in to take care of the month of may
+    %which has a space at it's end.
+    path2=[root,'\',year,'\',month];
+    if ~exist(path2, 'dir')
        mkdir(path2)
-end
-cd(path2);
-filename=[path2,'\',day,'.png'];
-filename2=[path2,'\',day,'AR.mat'];
-
-
-saveas(f,filename)
-save(filename2,'A','R');
-end
-
+    end
+    cd(path2);
+    filename=[path2,'\',day,'.png'];
+    filename2=[path2,'\',day,'AR.mat'];
+    
+    
+    saveas(f,filename) %this is a .png for the portion of globe you are interested saved according to the projection used here which is  EPSG:4326. you can always change the projection later, but that is of course another can of beans.
+    save(filename2,'A','R'); %the data that is save here is an matrix of colors (A) and a raster file (R). With those the world is pretty much your oyster.  
 end
 
-% some text
-% teoms dsf
+end
+
--- mydates_satretrieval_v2.m
+++ mydates_satretrieval_v2.m
@@ -1,17 +1,34 @@
+%This script is saved to isreister/Reister_etal2023 github account as
+%mydates_satretrieval_v2.m
+
+%This script is where you can select what satillite dates you want to
+%retreive from the aqua and terra MODIS satillites. IT then runs the
+%function WMSsatretrieval.m which does the heavy lifting of deciding which
+%if the satillite images are worth keeping given the cloud cover, as well
+%as creating a directory and saving the image in that directory. Search for
+%root in WMSsatretrieval to see the directory path.
+
 clear all
 %all of this is 250 m resolution.
 %note there was an error in aqua from october 22 to october 31 of 2005.
 %These dates were skipped. Terra does have some good data for some of those
 %dates.
-cd('C:\Users\cfosadmin\Documents\MATLAB\Research\code\API'); 
-load('dates.mat'); %we load up our remaining dates here;
-tseg=t; %we take the first segment being index 1:36 (days))
+cd('C:\Users\funkb\Documents\MATLAB\Research\code\API'); % Note that this location is not where the satillite images will be saved, you'll see that in the WMSsatretrieval function.
+
+t1 = datetime(2004,05,21,'Format','yyyy-MM-dd');% Make datetime vector for date span you are intersted in.
+t2 = datetime(2010,12,31,'Format','yyyy-MM-dd');
+tseg = t1:t2;
+
 %finding the land mask first
 
 
-landmaskdate=datetime(2018,06,1,'Format','yyyy-MM-dd'); %arbitrary choice of landmaskdate. This retreives one instance of landmask that we apply to the analysis of each image. Land doesn't change to much here and perspective is identical in all images.
+landmaskdate=datetime(2018,06,1,'Format','yyyy-MM-dd'); %arbitrary choice of landmaskdate. This retreives one instance of landmask that we apply to the analysis of each image. 
 strlandmaskdate=string(landmaskdate);% gets a string version of our date time. So we can use strcat later.
-url = 'https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?'; 
+
+url1 = 'https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?SERVICE=WMS&REQUEST=GetMap&VERSION=1.3.0&LAYERS=MODIS_Terra_MODIS_Terra_Cloud_Fraction_Day&STYLES=&FORMAT=image%2Fpng&TRANSPARENT=true&HEIGHT=256&WIDTH=256&TIME=';
+url2= strcat(strlandmaskdate, '&CRS=EPSG:4326&BBOX=-22.5,0,0,22.5'); 
+url= char(strcat(url1,url2));
+%url = 'https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?'; 
 landmaskinfo = wmsinfo(url);
 landmask = landmaskinfo.Layer.refine('osm');
 landmask = landmask.refine('OSM_Land_Mask','SearchField','LayerName');
@@ -20,19 +37,22 @@ lonlim = [-147,-144];
 imagelength = 2048;
 [LA,LR] = wmsread(landmask,'ImageHeight',imagelength,'ImageWidth',imagelength,...
     'Latlim',latlim,'Lonlim',lonlim,'ImageFormat','image/png');
-land=find(LA~=0);
+imshow(LA)
+land=find(double(LA(:,:,1))==75);
+% testing=zeros(2048,2048);
+% testing(land)=1;
+% imshow(testing);
 clear landmaskinfo;
 clear LA;
 
 for i=1:length(tseg)
     
 
-   cd('C:\Users\cfosadmin\Documents\MATLAB\Research\code\API'); 
-   WMSsatretrieval(land,tseg,i); 
+   cd('C:\Users\funkb\Documents\MATLAB\Research\code\API'); 
+   WMSsatretrievalv2(land,tseg,i); 
     
 end
 
-t(1:36)=[]; %we delete the days we just calculated from our date time values;
+
 cd('C:\Users\cfosadmin\Documents\MATLAB\Research\code\API'); 
-save('dates.mat','t');%resave t, replacing the one we loaded at the beginning, in preperation
-%for the next loading
+save('dates.mat','t');%resave t
